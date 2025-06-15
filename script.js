dfb809db0f3c23dbdce50661b704f50ddea219ea
vyazik1988 <ivan.anfalov@ue-germany.de>
2025-06-15 19:42:51 +0200

    fix

--- script.js
+++ script.js
@@ -45,11 +45,11 @@ document.addEventListener('DOMContentLoaded', () => {
         modalLinksContainer.innerHTML = '';
         if (itchUrl) {
             // This now uses an <img> tag. Make sure 'images/itch-logo.jpg' exists!
-            modalLinksContainer.innerHTML += `<a href="${itchUrl}" target="_blank" class="modal-link-btn itch"><img src="Logo/itch.jpg" alt="itch.io logo" class="link-icon"></a>`;
+            modalLinksContainer.innerHTML += `<a href="${itchUrl}" target="_blank" class="modal-link-btn itch"><img src="Logo/itch.png" alt="itch.io logo" class="link-icon"></a>`;
         }
         if (youtubeUrl) {
             // This now uses an <img> tag. Make sure 'images/youtube-logo.jpg' exists!
-            modalLinksContainer.innerHTML += `<a href="${youtubeUrl}" target="_blank" class="modal-link-btn youtube"><img src="Logo/yt.jpg" alt="YouTube logo" class="link-icon">YouTube</a>`;
+            modalLinksContainer.innerHTML += `<a href="${youtubeUrl}" target="_blank" class="modal-link-btn youtube"><img src="Logo/yt.png" alt="YouTube logo" class="link-icon"></a>`;
         }
         
         // Reset and setup slider
